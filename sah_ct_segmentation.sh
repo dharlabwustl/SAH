ff6b27ff851ed00a374268e91240e885153ffe88
dharlabwustl <atulkumar@wustl.edu>
2023-07-06 16:17:26 -0500

    Commit 2

--- sah_ct_segmentation.sh
+++ sah_ct_segmentation.sh
@@ -177,7 +177,7 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
           echo URI_1::${URI_1}
           filename=${array1[8]}
           echo filename::${filename}
-          call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${URI_1} ${filename} ${yasheng_code_input_dir})
+          call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${URI_1} ${filename} ${working_dir})
           outputfiles_present=$(/opt/conda/envs/pytorch1.12/bin/python download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
           #          /software/SAH_SEGMEN_FROM_YASHENG/ppredict.sh
         fi
