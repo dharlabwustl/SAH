8ad5925f7218c4d06ae15dffefc0c6916535edd6
dharlabwustl <atulkumar@wustl.edu>
2023-07-06 16:08:17 -0500

    Commit 2

--- sah_ct_segmentation.sh
+++ sah_ct_segmentation.sh
@@ -174,7 +174,7 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
 ################################################
     while IFS=',' read -ra array1; do
           URI_1=${array1[8]}  #if [[ "$string" == *"$Substring"* ]]
-          echo URI_1_1::${URI_1}
+#          echo URI_1_1::${URI_1}
           if [[  ${URI_1} == *"seg.nii.gz"* ]] | [[ ${URI_1} == *"normalized.nii.gz"* ]] ; then
                       echo URI_1::${URI_1}
           fi
