70cf78825e1627ac7ab107a110f70fc8b3ef89d8
dharlabwustl <atulkumar@wustl.edu>
2023-07-06 11:55:35 -0500

    Commit 2

--- builddockerimage.sh
+++ builddockerimage.sh
@@ -15,5 +15,5 @@ imagename=${2} #'yashengstep1'
 #docker build -t sharmaatul11/${imagename} ${parent_dir}
 #docker push sharmaatul11/${imagename}
 #
-#docker build -t registry.nrg.wustl.edu/docker/nrg-repo/sharmaatul11/${imagename} ${parent_dir}
+docker build -t registry.nrg.wustl.edu/docker/nrg-repo/sharmaatul11/${imagename} ${parent_dir}
 #docker push registry.nrg.wustl.edu/docker/nrg-repo/sharmaatul11/${imagename}
