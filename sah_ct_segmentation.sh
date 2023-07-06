e354fc793d98444abe567c337bfb70825315e35a
dharlabwustl <atulkumar@wustl.edu>
2023-07-06 12:50:37 -0500

    Commit 2

--- sah_ct_segmentation.sh
+++ sah_ct_segmentation.sh
@@ -141,24 +141,24 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
   #  outputfiles_present=0
   echo $niftifile_csvfilename
   outputfiles_present=0
-  while IFS=',' read -ra array; do
-    scanID=${array[2]}
-
-#    echo sessionId::${sessionID}
-#    echo scanId::${scanID}
-        echo ${array[3]}
-        echo ${array[4]}
-        echo ${array[1]}
-    resource_foldername="SAH_SEGM"
-    ### check if the file exists:
-    call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${resource_foldername} _resaved.nii.gz _resaved_4DL_normalized.nii.gz _resaved_levelset.nii.gz _resaved_4DL_seg.nii.gz _resaved_levelset_bet.nii.gz manual_splits.txt _resaved_4DL_normalized.nii.gz_csf_3.nii.gz _resaved_4DL_normalized.nii.gz_infarct.nii.gz _resaved_4DL_normalized.nii.gz_csf_4.nii.gz _resaved_4DL_normalized.nii.gz_csf_8.nii.gz _resaved_4DL_normalized.nii.gz_csf_1.nii.gz _resaved_4DL_normalized.nii.gz_csf_6.nii.gz _resaved_4DL_normalized.nii.gz_csf_2.nii.gz _resaved_4DL_normalized.nii.gz_csf_5.nii.gz _resaved_4DL_normalized.nii.gz_csf_7.nii.gz _resaved_4DL_normalized.nii.gz_csf_9.nii.gz _resaved_4DL_normalized.nii.gz_csf_10.nii.gz)
-    outputfiles_present=$(/opt/conda/envs/pytorch1.12/bin/python download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}")
-  done < <(tail -n +2 "${niftifile_csvfilename}")
-  ################################################
+#  while IFS=',' read -ra array; do
+#    scanID=${array[2]}
+#
+##    echo sessionId::${sessionID}
+##    echo scanId::${scanID}
+#        echo ${array[3]}
+#        echo ${array[4]}
+#        echo ${array[1]}
+#    resource_foldername="SAH_SEGM"
+#    ### check if the file exists:
+#    call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${resource_foldername} _resaved.nii.gz _resaved_4DL_normalized.nii.gz _resaved_levelset.nii.gz _resaved_4DL_seg.nii.gz _resaved_levelset_bet.nii.gz manual_splits.txt _resaved_4DL_normalized.nii.gz_csf_3.nii.gz _resaved_4DL_normalized.nii.gz_infarct.nii.gz _resaved_4DL_normalized.nii.gz_csf_4.nii.gz _resaved_4DL_normalized.nii.gz_csf_8.nii.gz _resaved_4DL_normalized.nii.gz_csf_1.nii.gz _resaved_4DL_normalized.nii.gz_csf_6.nii.gz _resaved_4DL_normalized.nii.gz_csf_2.nii.gz _resaved_4DL_normalized.nii.gz_csf_5.nii.gz _resaved_4DL_normalized.nii.gz_csf_7.nii.gz _resaved_4DL_normalized.nii.gz_csf_9.nii.gz _resaved_4DL_normalized.nii.gz_csf_10.nii.gz)
+#    outputfiles_present=$(/opt/conda/envs/pytorch1.12/bin/python download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}")
+#  done < <(tail -n +2 "${niftifile_csvfilename}")
+#  ################################################
   outputfiles_present=${outputfiles_present}0
   echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
   #echo "outputfiles_present::ATUL${outputfiles_present}::outputfiles_present"
-
+  outputfiles_present="0000000000"
   if [[ "${outputfiles_present: -1}" -eq 0 ]]; then
 
     echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
