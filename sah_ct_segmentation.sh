35fe70a9613789e4ba9614a2bc4b3d8c2a0f4e90
dharlabwustl <atulkumar@wustl.edu>
2023-07-06 15:27:04 -0500

    Commit 2

--- sah_ct_segmentation.sh
+++ sah_ct_segmentation.sh
@@ -173,7 +173,7 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
       outputfiles_present=$(/opt/conda/envs/pytorch1.12/bin/python download_with_session_ID.py "${call_get_resourcefiles_metadata_saveascsv_args_arguments[@]}")
 ################################################
     while IFS=',' read -ra array1; do
-          URI_1=${array1[1]}
+          URI_1=${array1[8]}
           echo ${URI_1}
     done < <(tail -n +2 "${dir_to_receive_the_data}/${output_csvfile}")
 
